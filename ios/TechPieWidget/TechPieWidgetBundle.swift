331f83960ea11c10eeeb9e125676167180385e02
MaoTechh <MaoTechMail@gmail.com>
2026-06-11 19:51:46 +0800

    feat: add widgets

---
+++ ios/TechPieWidget/TechPieWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  TechPieWidgetBundle.swift
+//  TechPieWidget
+//
+//  Created by Henry Li on 2026/6/11.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TechPieWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TechPieWidget()
+        TechPieWidgetControl()
+        TechPieWidgetLiveActivity()
+    }
+}
