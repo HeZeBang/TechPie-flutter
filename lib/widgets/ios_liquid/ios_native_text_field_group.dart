7ccfe55a4f10e39024cbf0dc40712c726c2df613
Honahec <honahec@gmail.com>
2026-07-31 20:35:27 +0800

    refactor(ios): use Cupertino text inputs

--- lib/widgets/ios_liquid/ios_native_text_field_group.dart
+++ lib/widgets/ios_liquid/ios_native_text_field_group.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import '../../utils/platform.dart';
 
@@ -25,177 +23,81 @@ class IosNativeTextFieldGroupItem {
   final ValueChanged<String>? onSubmitted;
 }
 
-class IosNativeTextFieldGroup extends StatefulWidget {
-  const IosNativeTextFieldGroup({
-    super.key,
-    required this.items,
-  });
+class IosNativeTextFieldGroup extends StatelessWidget {
+  const IosNativeTextFieldGroup({super.key, required this.items});
 
   final List<IosNativeTextFieldGroupItem> items;
-
-  @override
-  State<IosNativeTextFieldGroup> createState() =>
-      _IosNativeTextFieldGroupState();
-}
-
-class _IosNativeTextFieldGroupState extends State<IosNativeTextFieldGroup> {
-  MethodChannel? _channel;
-  bool _updatingFromNative = false;
-
-  @override
-  void initState() {
-    super.initState();
-    for (final item in widget.items) {
-      item.controller.addListener(_sendTextUpdate);
-    }
-  }
-
-  @override
-  void didUpdateWidget(covariant IosNativeTextFieldGroup oldWidget) {
-    super.didUpdateWidget(oldWidget);
-
-    for (final item in oldWidget.items) {
-      item.controller.removeListener(_sendTextUpdate);
-    }
-    for (final item in widget.items) {
-      item.controller.addListener(_sendTextUpdate);
-    }
-    unawaited(_sendConfigurationUpdate());
-  }
-
-  @override
-  void dispose() {
-    for (final item in widget.items) {
-      item.controller.removeListener(_sendTextUpdate);
-    }
-    _channel?.setMethodCallHandler(null);
-    _channel = null;
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     if (!isIos()) {
       return Column(
-        children: [
-          for (final item in widget.items)
-            TextField(
-              controller: item.controller,
-              decoration: InputDecoration(hintText: item.placeholder),
-              keyboardType: item.keyboardType,
-              textInputAction: item.textInputAction,
-              obscureText: item.obscureText,
-              enabled: item.enabled,
-              onSubmitted: item.onSubmitted,
-            ),
-        ],
+        children: [for (final item in items) _buildMaterialField(item)],
       );
     }
 
-    final separatorCount =
-        widget.items.length > 1 ? widget.items.length - 1 : 0;
-    return SizedBox(
-      height: widget.items.length * 56.0 + separatorCount * 0.5,
-      child: UiKitView(
-        viewType: _viewType,
-        layoutDirection: Directionality.of(context),
-        creationParams: _configuration,
-        creationParamsCodec: const StandardMessageCodec(),
-        onPlatformViewCreated: _onPlatformViewCreated,
+    final background =
+        CupertinoColors.secondarySystemGroupedBackground.resolveFrom(context);
+    final separator = CupertinoColors.separator.resolveFrom(context);
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: DecoratedBox(
+        decoration: BoxDecoration(color: background),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (var index = 0; index < items.length; index++) ...[
+              _buildCupertinoField(context, items[index]),
+              if (index < items.length - 1)
+                Padding(
+                  padding: const EdgeInsetsDirectional.only(start: 16),
+                  child: Container(height: 0.5, color: separator),
+                ),
+            ],
+          ],
+        ),
       ),
     );
   }
 
-  Map<String, Object?> get _configuration {
-    return <String, Object?>{
-      'items': [
-        for (final item in widget.items)
-          <String, Object?>{
-            'text': item.controller.text,
-            'placeholder': item.placeholder,
-            'keyboardType': _keyboardTypeName(item.keyboardType),
-            'textInputAction': item.textInputAction.name,
-            'obscureText': item.obscureText,
-            'enabled': item.enabled,
-          },
-      ],
-    };
-  }
-
-  void _onPlatformViewCreated(int viewId) {
-    final channel = MethodChannel('$_channelPrefix/$viewId');
-    _channel = channel;
-
-    channel.setMethodCallHandler((call) async {
-      final arguments = call.arguments as Map<Object?, Object?>?;
-      final index = arguments?['index'];
-      if (index is! int || index < 0 || index >= widget.items.length) {
-        return null;
-      }
-
-      switch (call.method) {
-        case 'onChanged':
-          final text = arguments?['text'] as String? ?? '';
-          final controller = widget.items[index].controller;
-          if (text == controller.text) return null;
-          _updatingFromNative = true;
-          controller.value = controller.value.copyWith(
-            text: text,
-            selection: TextSelection.collapsed(offset: text.length),
-            composing: TextRange.empty,
-          );
-          _updatingFromNative = false;
-        case 'onSubmitted':
-          final text = arguments?['text'] as String?;
-          final item = widget.items[index];
-          if (text != null && text != item.controller.text) {
-            _updatingFromNative = true;
-            item.controller.value = item.controller.value.copyWith(
-              text: text,
-              selection: TextSelection.collapsed(offset: text.length),
-              composing: TextRange.empty,
-            );
-            _updatingFromNative = false;
+  Widget _buildCupertinoField(
+    BuildContext context,
+    IosNativeTextFieldGroupItem item,
+  ) {
+    return SizedBox(
+      height: 56,
+      child: CupertinoTextField(
+        controller: item.controller,
+        placeholder: item.placeholder,
+        keyboardType: item.keyboardType,
+        textInputAction: item.textInputAction,
+        obscureText: item.obscureText,
+        enabled: item.enabled,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        decoration: const BoxDecoration(),
+        clearButtonMode: item.obscureText
+            ? OverlayVisibilityMode.never
+            : OverlayVisibilityMode.editing,
+        onSubmitted: (value) {
+          if (item.textInputAction == TextInputAction.next) {
+            FocusScope.of(context).nextFocus();
           }
-          item.onSubmitted?.call(item.controller.text);
-      }
-
-      return null;
-    });
-  }
-
-  void _sendTextUpdate() {
-    if (_updatingFromNative) return;
-    final channel = _channel;
-    if (channel == null) return;
-    unawaited(
-      channel.invokeMethod<void>('updateTexts', <String, Object?>{
-        'texts': [for (final item in widget.items) item.controller.text],
-      }),
+          item.onSubmitted?.call(value);
+        },
+      ),
     );
   }
 
-  Future<void> _sendConfigurationUpdate() async {
-    final channel = _channel;
-    if (channel == null) return;
-
-    try {
-      await channel.invokeMethod<void>('updateConfiguration', _configuration);
-    } on PlatformException {
-      // Platform view may be tearing down.
-    } on MissingPluginException {
-      // Platform view may not be wired yet.
-    }
-  }
-
-  String _keyboardTypeName(TextInputType keyboardType) {
-    if (keyboardType == TextInputType.emailAddress) return 'emailAddress';
-    if (keyboardType == TextInputType.phone) return 'phone';
-    if (keyboardType == TextInputType.url) return 'url';
-    if (keyboardType == TextInputType.number) return 'number';
-    return 'text';
+  Widget _buildMaterialField(IosNativeTextFieldGroupItem item) {
+    return TextField(
+      controller: item.controller,
+      decoration: InputDecoration(hintText: item.placeholder),
+      keyboardType: item.keyboardType,
+      textInputAction: item.textInputAction,
+      obscureText: item.obscureText,
+      enabled: item.enabled,
+      onSubmitted: item.onSubmitted,
+    );
   }
 }
-
-const _viewType = 'techpie/native_text_field_group';
-const _channelPrefix = 'techpie/native_text_field_group';
--- lib/widgets/ios_liquid/ios_native_text_view.dart
+++ lib/widgets/ios_liquid/ios_native_text_view.dart
@@ -1,11 +1,9 @@
-import 'dart:async';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import '../../utils/platform.dart';
 
-class IosNativeTextView extends StatefulWidget {
+class IosNativeTextView extends StatelessWidget {
   const IosNativeTextView({
     super.key,
     required this.controller,
@@ -26,130 +24,39 @@ class IosNativeTextView extends StatefulWidget {
   final bool enabled;
 
   @override
-  State<IosNativeTextView> createState() => _IosNativeTextViewState();
-}
-
-class _IosNativeTextViewState extends State<IosNativeTextView> {
-  MethodChannel? _channel;
-  bool _updatingFromNative = false;
-
-  @override
-  void initState() {
-    super.initState();
-    widget.controller.addListener(_sendTextUpdate);
-  }
-
-  @override
-  void didUpdateWidget(covariant IosNativeTextView oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.controller != widget.controller) {
-      oldWidget.controller.removeListener(_sendTextUpdate);
-      widget.controller.addListener(_sendTextUpdate);
-    }
-    unawaited(_sendConfigurationUpdate());
-  }
-
-  @override
-  void dispose() {
-    widget.controller.removeListener(_sendTextUpdate);
-    _channel?.setMethodCallHandler(null);
-    _channel = null;
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     if (!isIos()) {
       return TextField(
-        controller: widget.controller,
-        decoration: InputDecoration(
-          hintText: widget.placeholder,
-        ),
-        keyboardType: widget.keyboardType,
-        textInputAction: widget.textInputAction,
-        minLines: widget.minLines,
-        maxLines: widget.maxLines,
-        enabled: widget.enabled,
+        controller: controller,
+        decoration: InputDecoration(hintText: placeholder),
+        keyboardType: keyboardType,
+        textInputAction: textInputAction,
+        minLines: minLines,
+        maxLines: maxLines,
+        enabled: enabled,
       );
     }
 
-    return SizedBox(
-      height: 108,
-      child: UiKitView(
-        viewType: _viewType,
-        layoutDirection: Directionality.of(context),
-        creationParams: _configuration,
-        creationParamsCodec: const StandardMessageCodec(),
-        onPlatformViewCreated: _onPlatformViewCreated,
+    return ConstrainedBox(
+      constraints: const BoxConstraints(minHeight: 108),
+      child: CupertinoTextField(
+        controller: controller,
+        placeholder: placeholder,
+        keyboardType: keyboardType,
+        textInputAction: textInputAction,
+        minLines: minLines,
+        maxLines: maxLines,
+        enabled: enabled,
+        padding: const EdgeInsets.all(14),
+        textAlignVertical: TextAlignVertical.top,
+        clearButtonMode: OverlayVisibilityMode.editing,
+        decoration: BoxDecoration(
+          color: CupertinoColors.secondarySystemGroupedBackground.resolveFrom(
+            context,
+          ),
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
   }
-
-  Map<String, Object?> get _configuration {
-    return <String, Object?>{
-      'text': widget.controller.text,
-      'placeholder': widget.placeholder,
-      'keyboardType': _keyboardTypeName(widget.keyboardType),
-      'textInputAction': widget.textInputAction.name,
-      'minLines': widget.minLines,
-      'maxLines': widget.maxLines,
-      'enabled': widget.enabled,
-    };
-  }
-
-  void _onPlatformViewCreated(int viewId) {
-    final channel = MethodChannel('$_channelPrefix/$viewId');
-    _channel = channel;
-
-    channel.setMethodCallHandler((call) async {
-      final arguments = call.arguments as Map<Object?, Object?>?;
-      switch (call.method) {
-        case 'onChanged':
-          final text = arguments?['text'] as String? ?? '';
-          if (text == widget.controller.text) return null;
-          _updatingFromNative = true;
-          widget.controller.value = widget.controller.value.copyWith(
-            text: text,
-            selection: TextSelection.collapsed(offset: text.length),
-            composing: TextRange.empty,
-          );
-          _updatingFromNative = false;
-      }
-      return null;
-    });
-  }
-
-  void _sendTextUpdate() {
-    if (_updatingFromNative) return;
-    final channel = _channel;
-    if (channel == null) return;
-    unawaited(
-      channel.invokeMethod<void>('updateText', <String, Object?>{
-        'text': widget.controller.text,
-      }),
-    );
-  }
-
-  Future<void> _sendConfigurationUpdate() async {
-    final channel = _channel;
-    if (channel == null) return;
-    try {
-      await channel.invokeMethod<void>('updateConfiguration', _configuration);
-    } on PlatformException {
-      // Platform view may be tearing down.
-    } on MissingPluginException {
-      // Platform view may not be wired yet.
-    }
-  }
-
-  String _keyboardTypeName(TextInputType keyboardType) {
-    if (keyboardType == TextInputType.emailAddress) return 'emailAddress';
-    if (keyboardType == TextInputType.phone) return 'phone';
-    if (keyboardType == TextInputType.url) return 'url';
-    if (keyboardType == TextInputType.number) return 'number';
-    return 'text';
-  }
 }
-
-const _viewType = 'techpie/native_text_view';
-const _channelPrefix = 'techpie/native_text_view';
